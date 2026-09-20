96b0f2e23ee3ed106443a79cf789f7d976dc0ad7
Soheil <soheilnamdar@yahoo.com>
2026-09-20 11:29:04 +0200

    Add NAT gateway and private subnet routing

--- lab3-vpc-networking/main.tf
+++ lab3-vpc-networking/main.tf
@@ -29,3 +29,52 @@ resource "aws_subnet" "public" {
   vpc_id     = aws_vpc.lab3.id
   cidr_block = "10.0.1.0/24"
 }
+
+resource "aws_subnet" "private" {
+  vpc_id     = aws_vpc.lab3.id
+  cidr_block = "10.0.2.0/24"
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.lab3.id
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.lab3.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.lab3.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_eip" "nat" {
+  domain = "vpc"
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public.id
+
+  depends_on = [aws_internet_gateway.igw]
+}
